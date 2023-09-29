ff04d7de5bce913962f77ee67ae7b2aa9a34cf93
Ayzaz.Hussain@igd.com <Ayzaz.Hussain@igd.com>
2023-09-29 11:04:46 +0100

    end demo

--- func-testingGithubactions/__init__.py
+++ func-testingGithubactions/__init__.py
@@ -19,6 +19,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully by GithubActions.")
     else:
         return func.HttpResponse(
-             "This Sprint Review Demo is in progress!",
+             "This Sprint Review Demo has ended!",
              status_code=200
         )
